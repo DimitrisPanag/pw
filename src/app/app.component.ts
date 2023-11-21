71482404641b26d873efe43cec7703977dea355a
DimiPanag <76620584+DimitrisPanag@users.noreply.github.com>
2023-11-21 18:25:02 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Event, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(target: EventTarget){
+    const value = (<HTMLInputElement>target).value;
+    const parsedValue = parseInt(value);
+
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeUseSymbols(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+
+  onButtonClick(){
+    const Numbers = '1234567890';
+    const Letters = 'abcdefghijklmnopqrstuvxwyz';
+    const Symbols = '!@#$%^&*()'
+  }
+
+}
